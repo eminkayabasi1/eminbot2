04797db6e2c0e77954ee748802f34c421af4689c
Emin <emin.kayabasi@vbt.com.tr>
2024-12-28 12:11:33 +0300

    ty drone

--- untitled/src/main/java/com/app/fku/trendyol/fonksiyon/impl/TrendyolJSONServiceImpl.java
+++ untitled/src/main/java/com/app/fku/trendyol/fonksiyon/impl/TrendyolJSONServiceImpl.java
@@ -67,7 +67,7 @@ public class TrendyolJSONServiceImpl implements TrendyolJSONService {
         urlList.add(new LinkModel("https://public.trendyol.com/discovery-web-searchgw-service/v2/api/infinite-scroll/sr?wb=325%2C651%2C326%2C105973%2C653%2C103138&wc=1354&mb=kurumsal_satici&sst=PRICE_BY_ASC", 0.95d));//
         urlList.add(new LinkModel("https://public.trendyol.com/discovery-web-searchgw-service/v2/api/infinite-scroll/sr?wb=103138%2C325%2C326&wc=104593&mb=kurumsal_satici&sst=PRICE_BY_ASC", 0.95d));//
         urlList.add(new LinkModel("https://public.trendyol.com/discovery-web-searchgw-service/v2/api/infinite-scroll/sr?wb=104725%2C104764%2C467&wc=90&mb=kurumsal_satici&sst=PRICE_BY_ASC", 0.95d));//
-        urlList.add(new LinkModel("https://public.trendyol.com/discovery-web-searchgw-service/v2/api/infinite-scroll/sr?wb=109092&wc=108931&mb=kurumsal_satici&sst=PRICE_BY_ASC", 0.95d));//
+        urlList.add(new LinkModel("https://public.trendyol.com/discovery-web-searchgw-service/v2/api/infinite-scroll/sr?wb=109092&wc=164208&mb=kurumsal_satici&sst=PRICE_BY_ASC", 0.95d));//
 
 
         for (; ; ) {
